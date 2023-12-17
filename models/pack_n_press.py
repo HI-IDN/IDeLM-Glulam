7c50d7bea7e6a376903b99c57638d5d353d2e326
YourName <your@mail.com>
2023-12-17 09:50:01 +0000

    added new objective F

--- models/pack_n_press.py
+++ models/pack_n_press.py
@@ -88,9 +88,9 @@ def pack_n_press(merged, nump, debug=True):
     pmodel.addConstrs(Lp[k,r] >= x1[j, k, r] * L[j] for j in J for r in R for k in K)
 
     # make sure that all pattern length in region 1 are smaller than those in region 0
-    pmodel.addConstrs(x1[i, k, 0] * L[i] >= x1[j, k, 1] * L[j] - (1-x1[i, k, 0])*bigM - (1-x1[j, k, 1])*bigM for i in I for j in J for k in K)
+    #pmodel.addConstrs(x1[i, k, 0] * L[i] >= x1[j, k, 1] * L[j] - (1-x1[i, k, 0])*bigM - (1-x1[j, k, 1])*bigM for i in I for j in J for k in K)
     # make sure that the length of region 0 is longer than region 1
-    ####pmodel.addConstrs(Lp[k,0] >= Lp[k,1] - (1-z[k,1])*bigM for k in K)
+    pmodel.addConstrs(Lp[k,0] >= Lp[k,1] - (1-z[k,1])*bigM for k in K)
     pmodel.addConstrs((Lp[k,r] - L[j])*H[j] / 1000. / 1000.  <= F[j,k,r] + (1-x1[j,k,r])*bigM for j in J for k in K for r in R)
     # now we add the objective function as the sum of waste for all presses and the difference between demand and supply
     pmodel.setObjective(
@@ -116,7 +116,7 @@ def pack_n_press(merged, nump, debug=True):
         for r in R:
           for j in J:
               if x[j, k, r].X > 0.1:
-                  Lp_[k, r] = max(Lp_[k, r], L[j])
+                  Lp_[k, r] = int(max(Lp_[k, r], L[j]))
                   Waste_[k, r] += H[j] * (Lp[k, r].X - L[j]) * x[j, k, r].X / 1000 / 1000  # convert to m^2
   # print the solution
     if debug == True:
