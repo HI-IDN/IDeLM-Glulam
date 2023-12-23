2fbf4a7a5141d9fe7168ebcdc4857f688780f79d
YourName <your@mail.com>
2023-12-23 08:26:59 +0000

    what are the settings for the solver?

---
+++ utils/get_gurobi_params.py
@@ -0,0 +1,45 @@
+import gurobipy as gp
+import pickle
+
+# Create a Gurobi model
+model = gp.Model()
+
+# List of parameter names
+parameter_names = [
+    'AggFill', 'Aggregate', 'BarConvTol', 'BarCorrectors', 'BarHomogeneous', 'BarIterLimit', 'BarOrder', 'BarQCPConvTol', 'BestBdStop', 'BestObjStop',
+    'BQPCuts', 'BranchDir', 'CliqueCuts', 'CloudAccessID', 'CloudHost', 'CloudPool', 'CloudSecretKey', 'ComputeServer', 'ConcurrentJobs', 'ConcurrentMethod',
+    'ConcurrentMIP', 'CoverCuts', 'Crossover', 'CrossoverBasis', 'CSAPIAccessID', 'CSAPISecret', 'CSAppName', 'CSAuthToken', 'CSBatchMode', 'CSClientLog',
+    'CSGroup', 'CSIdleTimeout', 'CSManager', 'CSPriority', 'CSQueueTimeout', 'CSRouter', 'CSTLSInsecure', 'CutAggPasses', 'Cutoff', 'CutPasses',
+    'Cuts', 'DegenMoves', 'Disconnected', 'DisplayInterval', 'DistributedMIPJobs', 'DualReductions', 'Dummy', 'FeasibilityTol', 'FeasRelaxBigM', 'FlowCoverCuts',
+    'FlowPathCuts', 'FuncMaxVal', 'FuncNonlinear', 'FuncPieceError', 'FuncPieceLength', 'FuncPieceRatio', 'FuncPieces', 'GomoryPasses', 'GUBCoverCuts',
+    'Heuristics', 'IgnoreNames', 'IISMethod', 'ImpliedCuts', 'ImproveStartGap', 'ImproveStartNodes', 'ImproveStartTime', 'InfProofCuts', 'InfUnbdInfo',
+    'IntegralityFocus', 'IntFeasTol', 'IterationLimit', 'JobID', 'JSONSolDetail', 'LazyConstraints', 'LicenseID', 'LiftProjectCuts', 'LogFile', 'LogToConsole',
+    'LPWarmStart', 'MarkowitzTol', 'MemLimit', 'Method', 'MinRelNodes', 'MIPFocus', 'MIPGap', 'MIPGapAbs', 'MIPSepCuts', 'MIQCPMethod', 'MIRCuts',
+    'MixingCuts', 'ModKCuts', 'MultiObjMethod', 'MultiObjPre', 'NetworkAlg', 'NetworkCuts', 'NLPHeur', 'NodefileDir', 'NodefileStart', 'NodeLimit',
+    'NodeMethod', 'NonConvex', 'NoRelHeurTime', 'NoRelHeurWork', 'NormAdjust', 'NumericFocus', 'OBBT', 'ObjNumber', 'ObjScale', 'OptimalityTol',
+    'OutputFlag', 'PartitionPlace', 'PerturbValue', 'PoolGap', 'PoolGapAbs', 'PoolSearchMode', 'PoolSolutions', 'PreCrush', 'PreDepRow', 'PreDual',
+    'PreMIQCPForm', 'PrePasses', 'PreQLinearize', 'Presolve', 'PreSOS1BigM', 'PreSOS1Encoding', 'PreSOS2BigM', 'PreSOS2Encoding', 'PreSparsify', 'ProjImpliedCuts',
+    'PSDCuts', 'PSDTol', 'PumpPasses', 'QCPDual', 'Quad', 'Record', 'RelaxLiftCuts', 'ResultFile', 'RINS', 'RLTCuts', 'ScaleFlag', 'ScenarioNumber',
+    'Seed', 'ServerPassword', 'ServerTimeout', 'Sifting', 'SiftMethod', 'SimplexPricing', 'SoftMemLimit', 'SolFiles', 'SolutionLimit', 'SolutionNumber',
+    'SolutionTarget', 'StartNodeLimit', 'StartNumber', 'StrongCGCuts', 'SubMIPCuts', 'SubMIPNodes', 'Symmetry', 'Threads', 'TimeLimit', 'TokenServer',
+    'TSPort', 'TuneCleanup', 'TuneCriterion', 'TuneDynamicJobs', 'TuneJobs', 'TuneMetric', 'TuneOutput', 'TuneResults', 'TuneTargetMIPGap', 'TuneTargetTime',
+    'TuneTimeLimit', 'TuneTrials', 'UpdateMode', 'Username', 'VarBranch', 'WLSAccessID', 'WLSSecret', 'WLSToken', 'WLSTokenDuration', 'WLSTokenRefresh',
+    'WorkerPassword', 'WorkerPool', 'WorkLimit', 'ZeroHalfCuts', 'ZeroObjNodes'
+]
+
+Info = {}
+for param in parameter_names:
+    # Get parameter info
+    try:
+        info = model.getParamInfo(param)
+        Info[param] = info
+        #print(f"Parameter: {param}")
+        #print(f"  Current value: {info[0]}")
+        #print(f"  Default value: {info[3]}")
+        #print(f"  Min value: {info[1]}")
+        #print(f"  Max value: {info[2]}")
+    except gp.GurobiError as e:
+        print(f"Error with parameter {param}: {e}")
+
+with open('gurobi_settings.pkl', 'wb') as file: 
+    pickle.dump(Info, file)
